1be0eb46e6f03ea0edb508dd86a942c3c0748197
Abderrahmane Melhaoui <melhaoui.abderrahmane@gmail.com>
2021-10-04 13:51:56 +0100

    add sentry on frontend

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -3,11 +3,22 @@ import ReactDOM from "react-dom";
 import { ApolloProvider } from "@apollo/client";
 import { ThemeProvider } from "@mui/material/styles";
 
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
 import client from "app/graphQL/Client";
 import App from "app/App";
 import theme from "theme";
 
 import "./index.css";
+
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_URL,
+  integrations: [new Integrations.BrowserTracing()],
+
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
